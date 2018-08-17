0404b8a103c3288bdd6a4fbaf6830e6cee463884
Johnny_GM <42081527+gomezmarinjohnny@users.noreply.github.com>
2018-08-16 22:41:38 -0500

    Punto 5
    ESTA APLICACIÓN COMPARA 3 NÚMEROS FLOTANTES Y DETERMINA CUAL ES EL MAYOR

---
+++ punto5.cpp
@@ -0,0 +1,26 @@
+// ESTA APLICACIÓN COMPARA 3 NÚMEROS FLOTANTES
+// Y DETERMINA CUAL ES EL MAYOR
+
+#include <iostream>
+#include <string>
+
+
+
+int main()
+
+{
+  float numero1,numero2,numero3;
+  
+  std::cout << "INGRESE EL PRIMER NUMERO TIPO FLOTANTE" << std::endl;
+  std::cin >> numero1;
+  
+  std::cout <<"INGRESE EL SEGUNDO NUMERO TIPO FLOTANTE" << std::endl;
+  std::cin >> numero2;
+  
+  std::cout <<"INGRESE EL TERCER NUMERO TIPO FLOTANTE" << std::endl;
+  std::cin >> numero3;
+  
+  (numero1>numero2&&numero1>numero3)?std::cout<<"El numero mayor es: "<<numero1: std::cout<<" ";
+  (numero2>numero1&&numero2>numero3)?std::cout<<"El numero mayor es: "<<numero2: std::cout<<" ";
+  (numero3>numero1&&numero3<numero2)?std::cout<<"El numero mayor es: "<<numero3: std::cout<<" ";
+}
